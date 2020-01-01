1060ad61e7cfd0446774faf2ce5e7df6f636735e
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-01 16:38:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  sum = num2 + num1
+  puts sum
 end
 
 def subtraction(num1, num2)
+  difference = num1 - num2
+  puts difference
 
 end
 
 def division(num1, num2)
+  qoutient = num1 / num2
+  puts qoutient
 
 end
 
 def multiplication(num1, num2)
+  multiplication = num2 * num1
 
 end
 
 def modulo(num1, num2)
+  modulo= num1 % num2
+  puts modulo
+  
 
 end
 
 def square_root(num)
+  square_root = num **
+  puts square_root
 
 end
